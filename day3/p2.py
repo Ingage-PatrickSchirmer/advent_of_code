66ee22dd0010a67d6f10f65685a4a1d78dd2e064
Ingage-PatrickSchirmer <patrick.schirmer@ingagepartners.com>
2022-12-03 13:23:35 -0500

    edits after sleep, its hard staying up till 12am waiting to solve a coding challange. The brain doesnt want to be involved after 10pm

--- day3/p2.py
+++ day3/p2.py
@@ -1,21 +1,42 @@
 import string
 
 
-atoz = string.ascii_lowercase + string.ascii_uppercase
+pk = string.ascii_lowercase + string.ascii_uppercase
 
 
-with open("./day3/input.txt") as f:
+with open("input.txt") as f:
     inputs = f.read().splitlines()
 
 
 total1 = sum(
-    atoz.index("".join(set(pack[: len(pack) // 2]) & set(pack[len(pack) // 2 :]))) + 1
+    pk.index("".join(set(pack[: len(pack) // 2]) & set(pack[len(pack) // 2 :]))) + 1
     for pack in inputs
 )
 
 total2 = sum(
-    atoz.index("".join(set.intersection(*map(set, group)))) + 1
+    pk.index("".join(set.intersection(*map(set, group)))) + 1
     for group in (inputs[i : i + 3] for i in range(0, len(inputs), 3))
 )
 
-print(total1, total2)
+print(total1)
+print(total2)
+
+ruck = ["".join(set(pack[: len(pack) // 2]) & (set(pack[len(pack) // 2 :]))) for pack in inputs]
+# print(ruck)
+
+r1 = [set(pack[len(pack) // 2 :]) & (set(pack[len(pack) // 2 :])) for pack in inputs]
+# r2 = [set(pack[len(pack) // 2 :])for pack in inputs]
+
+# def check(a ,b):
+#     a_set = set(a)
+#     b_set = set(b)
+
+#     if (a_set & b_set):
+#         print(a_set & b_set)
+
+
+# for i in inputs:
+#     a1 = i[: len(i) // 2]
+#     b1 = i[len(i) // 2 :] 
+#     check(a1,b1)
+    
